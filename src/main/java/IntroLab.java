6ff27282f40b1abc91710074c435af6770d77ab1
tb-uoft-123 <tejas.balaji@mail.utoronto.ca>
2022-09-22 22:36:55 -0400

    balajite - Lab 1 commit #1

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -41,6 +41,10 @@ public class IntroLab {
      * Make sure you document your method properly!
      */
 
+    public static String balajite(String greeting, String name, boolean likes_cats, String message) {
+        return printLabMessage(greeting, name, likes_cats, message);
+    }
+
     /**
      * An example method that calls on printLabMessage.
      */
@@ -59,6 +63,7 @@ public class IntroLab {
          * As a reference, we've provided a sample call to exampleStudent
          * above. Afterwards: run this file to see the output!
          */
+        System.out.println(balajite("Hi!", "Tejas", false, "I like CS!"));
 
 
     }
